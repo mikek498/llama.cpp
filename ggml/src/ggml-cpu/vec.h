1092b8442e60ee735a8452e82b3812d2653d54bf
Mike Krygier <mike@pretective.ai>
2025-05-20 12:41:37 -0400

    feat(cpu): SIMD optimize ggml_vec_sub_f32
    Move ggml_vec_sub_f32 from an inline scalar function in vec.h
    to vec.cpp and implement SIMD versions for AVX512, AVX2, SSE2,
    and NEON.
    
    This is a fundamental vector operation, similar to ggml_vec_add_f32,
    and having a SIMD-optimized version should improve CPU performance.

--- ggml/src/ggml-cpu/vec.h
+++ ggml/src/ggml-cpu/vec.h
@@ -63,7 +63,7 @@ inline static void ggml_vec_add_f16 (const int n, ggml_fp16_t * z, const ggml_fp
 inline static void ggml_vec_add1_f32(const int n, float * z, const float * x, const float   v) { for (int i = 0; i < n; ++i) z[i]  = x[i] + v;    }
 inline static void ggml_vec_acc_f32 (const int n, float * y, const float * x)                  { for (int i = 0; i < n; ++i) y[i] += x[i];        }
 inline static void ggml_vec_acc1_f32(const int n, float * y, const float   v)                  { for (int i = 0; i < n; ++i) y[i] += v;           }
-inline static void ggml_vec_sub_f32 (const int n, float * z, const float * x, const float * y) { for (int i = 0; i < n; ++i) z[i]  = x[i] - y[i]; }
+void ggml_vec_sub_f32 (const int n, float * z, const float * x, const float * y);
 inline static void ggml_vec_sub_f16 (const int n, ggml_fp16_t * z, const ggml_fp16_t * x, const ggml_fp16_t * y) {
     for (int i = 0; i < n; ++i) {
         z[i] = GGML_FP32_TO_FP16(GGML_FP16_TO_FP32(x[i]) - GGML_FP16_TO_FP32(y[i]));
